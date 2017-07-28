8a2389d426da72d11de4d083628d3db6fe692de5
johnreybacal <30515324+johnreybacal@users.noreply.github.com>
2017-07-28 15:50:11 +0800

    Add files via upload

--- SAD/index.php
+++ SAD/index.php
@@ -4,27 +4,38 @@
 		<link rel = "stylesheet" type = "text/css" href = "views/css/default.css" />		
 	</head>
 	<body>
-		<?php
-			include "views/includes/menu1.php";
-		?>
-		
+		<div id="login">
+			<a href="">Login | Signup</a>
+		</div>
 		<div id="banner">
 			Insert banner here
 		</div>
-		<div id="login">
-			<a href="">Login</a>
+		<div id="bff">
+			Brighten flowers & fruits
 		</div>
-		
+		<?php
+			include "views/includes/menu1.php";
+		?>
+
 		<div id="navimage">
+			navigator logo
+		</div>
+		<div id="workspace">			
+			<div class="content">
+				-content
+			</div>
+			
+			<div class="content">
+				-content
+			</div>
+			
+			<div id="news">
+				news
+			</div>
 		</div>
 		
-		<div class="content">
-		</div>
-		
-		<div class="content">
-		</div>
-		
-		<div id="news">
+		<div id="footer">
+			footer
 		</div>
 	</body>
 </html>
