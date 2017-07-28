423d7324f1c57bf7fad7e5083e6a7eb47d910ba9
johnreybacal <30515324+johnreybacal@users.noreply.github.com>
2017-07-28 14:37:10 +0800

    Add files via upload

--- SAD/index.php
+++ SAD/index.php
@@ -1,8 +1,30 @@
 <html>
 	<head>
-		<title>BFF</title>
+		<title>BFF</title>		
+		<link rel = "stylesheet" type = "text/css" href = "views/css/default.css" />		
 	</head>
 	<body>
-		<?php echo "Yo Guys!"; ?>
+		<?php
+			include "views/includes/menu1.php";
+		?>
+		
+		<div id="banner">
+			Insert banner here
+		</div>
+		<div id="login">
+			<a href="">Login</a>
+		</div>
+		
+		<div id="navimage">
+		</div>
+		
+		<div class="content">
+		</div>
+		
+		<div class="content">
+		</div>
+		
+		<div id="news">
+		</div>
 	</body>
 </html>
---
+++ SAD/views/includes/menu1.php
@@ -0,0 +1,9 @@
+<div id="navigator">
+	<ul>
+		<li><a href="">Home</a></li>
+		<li><a href="">Place an order</a></li>
+		<li><a href="">Services</a></li>
+		<li><a href="">About Us</a></li>
+		<li><a href="">Contact Us</a></li>
+	</ul>
+</div>
