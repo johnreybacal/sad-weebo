a88ade906ae01dbffb6e60f4772183aea529d56e
johnreybacal <30515324+johnreybacal@users.noreply.github.com>
2017-07-28 16:49:32 +0800

    Add files via upload

--- SAD/views/includes/menu1.php
+++ SAD/views/includes/menu1.php
@@ -1,9 +1,10 @@
-<div id="navigator">
-	<ul>
-		<li><a href="">Home</a></li>
-		<li><a href="">Place an order</a></li>
-		<li><a href="">Services</a></li>
-		<li><a href="">About Us</a></li>
-		<li><a href="">Contact Us</a></li>
-	</ul>
-</div>
+
+	<div id="navigator">
+		<ul>
+			<li><a href="home.php">Home</a></li>
+			<li><a href="">Place an order</a></li>
+			<li><a href="">Services</a></li>
+			<li><a href="">About Us</a></li>
+			<li><a href="">Contact Us</a></li>
+		</ul>
+	</div>
--- SAD/views/signup.php
+++ SAD/views/signup.php
@@ -1,16 +1,17 @@
 <html>
 	<head>
 		<title>BFF</title>		
-		<link rel = "stylesheet" type = "text/css" href = "../views/css/default.css" />		
+		<link rel = "stylesheet" type = "text/css" href = "../views/css/signup.css" />		
 	</head>
 	<body>
 		<div id="login">
-				<a href="signup.php">Signup</a>
-				<a href="">Login</a>
+			<a href="">Login</a>
 		</div>
+		
 		<div id="banner">
 			Insert banner here
 		</div>
+		
 		<div id="bff">
 			Brighten flowers & fruits
 		</div>
@@ -26,12 +27,17 @@
 				-content
 			</div>
 			
-			<div class="content">
-				-content
-			</div>
-			
-			<div id="news">
-				news
+			<div id="sign">
+				<form id="signform" action="" method="get">
+					Username:
+					<input type="text" name="username"><br />
+					First Name:
+					<input type="text" name="firstname"><br />
+					Last Name:
+					<input type="text" name="lastname"><br />
+					Password:
+					<input type="password" name="password">
+				</form>
 			</div>
 		</div>
 		
