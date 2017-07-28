bb466e92521c77537c4e95a6ec4c602d1d96e9b4
johnreybacal <30515324+johnreybacal@users.noreply.github.com>
2017-07-28 15:54:12 +0800

    Add files via upload

--- SAD/views/home.php
+++ SAD/views/home.php
@@ -1,3 +1,41 @@
-<div>
-	content here
-</div>
+<html>
+	<head>
+		<title>BFF</title>		
+		<link rel = "stylesheet" type = "text/css" href = "views/css/default.css" />		
+	</head>
+	<body>
+		<div id="login">
+			<a href="">Login | Signup</a>
+		</div>
+		<div id="banner">
+			Insert banner here
+		</div>
+		<div id="bff">
+			Brighten flowers & fruits
+		</div>
+		<?php
+			include "views/includes/menu1.php";
+		?>
+
+		<div id="navimage">
+			navigator logo
+		</div>
+		<div id="workspace">			
+			<div class="content">
+				-content
+			</div>
+			
+			<div class="content">
+				-content
+			</div>
+			
+			<div id="news">
+				news
+			</div>
+		</div>
+		
+		<div id="footer">
+			footer
+		</div>
+	</body>
+</html>
