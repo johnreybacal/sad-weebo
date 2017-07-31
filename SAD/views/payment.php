37f063ed1138e745842f7a13c8d11018b5f93223
Abdul Jabbar <30515378+naytanyel@users.noreply.github.com>
2017-07-31 19:41:05 +0800

    Update payment.php

--- SAD/views/payment.php
+++ SAD/views/payment.php
@@ -3,8 +3,6 @@
 margin:0% 0% 1% 0%;
 }
 </style>
-<div>
-	
 	<div>
 		<fieldset>	
 			<h3>Payment</h3><hr/>
@@ -16,21 +14,16 @@ margin:0% 0% 1% 0%;
 				<input type="radio" name="Cardname" id="paypal"/>
 					<label for="visa"><img src="#" alt="paypal"/> </label>
 				<input type="radio" name="Cardname" id="ebay"/>
-					<label for="visa"><img src="#" alt="ebay"/> </label>			
-			</form>
-			<form >
+					<label for="visa"><img src="#" alt="ebay" /> </label>	
+				<br/><br/>
 				Card Number			<input type="text" placeholder="Card Number"id="id"/><br/>
 				Cardholder's Name	<input type="text" placeholder="Cardholder's Name"id="id"/><br/>
 				Expiry Date			<input type="text" placeholder="mm" style = "width:2%;"id="id"/>
 									<input type="text" placeholder="yy" style = "width:2%;"id="id"/><br/>
-				<input type="checkbox" id="id"/> &nbsp; Do not save this card <br/>
-				<input type="submit" href= "#" value="Place Your Order" style="margin-left:5%;"/>
+				CVV					<input type="text" placeholder="CVV" id="id"/><br/>
+									<input type="checkbox" id="id"/> &nbsp; Do not save this card <br/>
+									<input type="submit" href= "#" value="Place Your Order" style="margin-left:5%;"/>
 			</form>
 			
 		</fieldset>	
 	</div>
-	<div>
-	</div>
-	<div>
-	</div>
-</div>
