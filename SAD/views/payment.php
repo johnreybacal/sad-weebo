670384eab5b1399659b54d68766df3ca3f0886ea
Abdul Jabbar <30515378+naytanyel@users.noreply.github.com>
2017-07-31 19:21:21 +0800

    Add files via upload

---
+++ SAD/views/payment.php
@@ -0,0 +1,36 @@
+<style>
+#id{
+margin:0% 0% 1% 0%;
+}
+</style>
+<div>
+	
+	<div>
+		<fieldset>	
+			<h3>Payment</h3><hr/>
+			<form >
+				<input type="radio" name="Cardname" id="visa"/>
+					<label for="visa"><img src="#" alt="visa"/> </label>
+				<input type="radio" name="Cardname" id="mastercard"/>
+					<label for="visa"><img src="#" alt="mastercard"/> </label>
+				<input type="radio" name="Cardname" id="paypal"/>
+					<label for="visa"><img src="#" alt="paypal"/> </label>
+				<input type="radio" name="Cardname" id="ebay"/>
+					<label for="visa"><img src="#" alt="ebay"/> </label>			
+			</form>
+			<form >
+				Card Number			<input type="text" placeholder="Card Number"id="id"/><br/>
+				Cardholder's Name	<input type="text" placeholder="Cardholder's Name"id="id"/><br/>
+				Expiry Date			<input type="text" placeholder="mm" style = "width:2%;"id="id"/>
+									<input type="text" placeholder="yy" style = "width:2%;"id="id"/><br/>
+				<input type="checkbox" id="id"/> &nbsp; Do not save this card <br/>
+				<input type="submit" href= "#" value="Place Your Order" style="margin-left:5%;"/>
+			</form>
+			
+		</fieldset>	
+	</div>
+	<div>
+	</div>
+	<div>
+	</div>
+</div>
